80b93a79fbb802ae8a7fe24ffe4cead054a28177
Fpress225 <102734788+Fpress225@users.noreply.github.com>
2022-05-31 08:46:03 +0300

    Update check_king.c

--- check_king.c
+++ check_king.c
@@ -1,101 +1,56 @@
 #include <stdbool.h>
 
-struct cots { // coordinates of the shapes
-
+typedef struct coordinates_of_the_king {
   int x, y;
-
-};
+} Pos;
 
 struct cots search_king(const char chessboard[8][8]) {
-
   for (int y = 0; y < 8; y++) 
-
     for (int x = 0; x < 8; x++)
-
-      if (chessboard[x][y] == 'K')
-
-        return struct cots k = {x, y}; // k = king
-
+      if (chessboard[y][x] == 'K')
+        return (Pos) {x, y}
 }
 
 bool check_horizontally(const char chessboard[8][8]) {
-
-  struct cots k = search_king(chessboard); // k = king
-
-  for (int k_x = k.x; k_x > -1; k_x--) {
-
-    if (chessboard[k_x][k.y] == 'N' || chessboard[k_x][k.y] == 'R' || chessboard[k_x][k.y] == 'P')
-
+  Pos king = search_king(chessboard);
+  for (int x = k.x; x > -1; x--) {
+    if (chessboard[king.y][x] == 'N' || chessboard[king.y][x] == 'R' || chessboard[king.y][x] == 'P')
       return false;
-
-    else if (chessboard[k_x][k.y] == 'B' ||  chessboard[k_x][k.y] == 'Q')
-
+    else if (chessboard[king.y][x] == 'B' ||  chessboard[king.y][x] == 'Q')
       return true;
-
   }
-
-  for (int k_x = k.x; k_x < 8; k_x++) {
-
-    if (chessboard[k_x][k.y] == 'N' || chessboard[k_x][k.y] == 'R' || chessboard[k_x][k.y] == 'P')
-
+  for (int x = k.x; x < 8; x++) {
+    if (chessboard[king.y][x] == 'N' || chessboard[king.y][x] == 'R' || chessboard[king.y][x] == 'P')
       return false;
-
-    else if (chessboard[k_x][k.y] == 'B' ||  chessboard[k_x][k.y] == 'Q')
-
+    else if (chessboard[king.y][x] == 'B' ||  chessboard[king.y][x] == 'Q')
       return true;
-
   }
-
   return false;
-
 }
 
 bool check_vertically(const char chessboard[8][8]) {
-
-  struct cots k = search_king(chessboard); // k = king
-
-  for (int k_y = k.y; k_y > -1; k_y--) {
-
-    if (chessboard[k.x][k_y] == 'N' || chessboard[k.x][k_y] == 'R' || chessboard[k.x][k_y] == 'P')
-
+  Pos king = search_king(chessboard);
+  for (int y = king.y; y > -1; y--) {
+    if (chessboard[y][king.x] == 'N' || chessboard[y][king.x] == 'R' || chessboard[y][king.x] == 'P')
       return false;
-
-    else if (chessboard[k.x][k_y] == 'B' ||  chessboard[k.x][k_y] == 'Q')
-
+    else if (chessboard[y][king.x] == 'B' ||  chessboard[y][king.x] == 'Q')
       return true;
-
   }
-
-  for (int k_y = k.y; k_y < 8; k_y++) {
-
-    if (chessboard[k.x][k_y] == 'N' || chessboard[k.x][k_y] == 'R' || chessboard[k.x][k_y] == 'P')
-
+  for (int y = king.y; y < 8; y++) {
+    if (chessboard[y][king.x] == 'N' || chessboard[y][king.x] == 'R' || chessboard[y][king.x] == 'P')
       return false;
-
-    else if (chessboard[k.x][k_y] == 'B' ||  chessboard[k.x][k_y] == 'Q')
-
+    else if (chessboard[y][king.x] == 'B' ||  chessboard[y][king.x] == 'Q')
       return true;
-
   }
-
   return false;
-
 }
 
 bool king_is_in_check (const char chessboard[8][8])
-
 {
-
   bool check_horiz = check_horizontally(chessboard);
-
-  bool check_vertic = check_vertically(chessboard);
-
-  if (check_horiz || check_vertic) {
-
+  bool check_vertic = check_vertically(chessboard)
+  if (check_horiz == true || check_vertic == true) {
     return true;
-
   }
-
   return false;
-
 }
