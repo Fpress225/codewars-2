790b46c281d051829c7836e7f7ac2fb50ae714af
Fpress225 <102734788+Fpress225@users.noreply.github.com>
2022-06-02 19:04:12 +0200

    Update check_king_test.c

--- check_king_test.c
+++ check_king_test.c
@@ -18,43 +18,43 @@ Pos search_queen(const char chessboard[8][8]) {
         return (Pos) {x, y};
 }
 
-int check_with_a_queen(int king_y, int king_x, const char chessboard[8][8]) {
+int check_with_a_queen(Pos king, const char chessboard[8][8]) {
   Pos queen = search_queen(chessboard);
-  for (int y = king_y, x = king_x; y < 8, x < 8; y++, x++) 
+  for (int y = king.y, x = king.x; y < 8, x < 8; y++, x++) 
     if (y == queen.y && x == queen.x) 
       return 1;
 
-  for (int y = king_y, x = king_x; y < 8, x > -1; y++, x--) 
+  for (int y = king.y, x = king.x; y < 8, x > -1; y++, x--) 
     if (y == queen.y && x == queen.x) 
       return 1;
 
-  for (int y = king_y, x = king_x; y > -1, x > -1; y--, x--) 
+  for (int y = king.y, x = king.x; y > -1, x > -1; y--, x--) 
     if (y == queen.y && x == queen.x) 
       return 1;
 
-  for (int y = king_y, x = king_x; y > -1, x < 8; y--, x++) 
+  for (int y = king.y, x = king.x; y > -1, x < 8; y--, x++) 
     if (y == queen.y && x == queen.x) 
       return 1;
 
-  if (queen.y == king_y) {
-    if (queen.y < king_y) {
-      for (int y = queen.y + 1; y < king_y; y++) 
-        if (chessboard[y][king_x] == 'R')
+  if (queen.y == king.y) {
+    if (queen.y < king.y) {
+      for (int y = queen.y + 1; y < king.y; y++) 
+        if (chessboard[y][king.x] == 'R')
           return 1;
-    } else if (queen.y > king_y) { 
-      for (int y = queen.y - 1; y > king_y; y--) 
-        if (chessboard[y][king_x] == 'R')
+    } else if (queen.y > king.y) { 
+      for (int y = queen.y - 1; y > king.y; y--) 
+        if (chessboard[y][king.x] == 'R')
           return 1;
     }
   } 
-  if (queen.x == king_x) {
-    if (queen.x < king_x) {
-      for (int x = queen.x + 1; x < king_x; x++) 
-        if (chessboard[king_y][x] == 'R')
+  if (queen.x == king.x) {
+    if (queen.x < king.x) {
+      for (int x = queen.x + 1; x < king.x; x++) 
+        if (chessboard[king.y][x] == 'R')
           return 1;
-    } else if (queen.x > king_x) {
-      for (int x = queen.x - 1; x > king_x; x--) 
-        if (chessboard[king_y][x] == 'R')
+    } else if (queen.x > king.x) {
+      for (int x = queen.x - 1; x > king.x; x--) 
+        if (chessboard[king.y][x] == 'R')
           return 1;
     }
   }
@@ -68,21 +68,21 @@ Pos search_bishop(const char chessboard[8][8]) {
         return (Pos) {x, y};
 }
 
-int check_with_a_bishop(int king_y, int king_x, const char chessboard[8][8]) {
+int check_with_a_bishop(Pos king, const char chessboard[8][8]) {
   Pos bishop = search_bishop(chessboard);
-  for (int y = king_y, x = king_x; y < 8, x < 8; y++, x++) 
+  for (int y = king.y, x = king.x; y < 8, x < 8; y++, x++) 
     if (y == bishop.y && x == bishop.x) 
       return 1;
 
-  for (int y = king_y, x = king_x; y < 8, x > -1; y++, x--) 
+  for (int y = king.y, x = king.x; y < 8, x > -1; y++, x--) 
     if (y == bishop.y && x == bishop.x) 
       return 1;
 
-  for (int y = king_y, x = king_x; y > -1, x > -1; y--, x--) 
+  for (int y = king.y, x = king.x; y > -1, x > -1; y--, x--) 
     if (y == bishop.y && x == bishop.x) 
       return 1;
 
-  for (int y = king_y, x = king_x; y > -1, x < 8; y--, x++) 
+  for (int y = king.y, x = king.x; y > -1, x < 8; y--, x++) 
     if (y == bishop.y && x == bishop.x) 
       return 1;
 }
@@ -94,38 +94,38 @@ Pos search_knignt(const char chessboard[8][8]) {
         return (Pos) {x, y};
 }
 
-int check_with_a_knight(int king_y, int king_x, const char chessboard[8][8]) {
+int check_with_a_knight(Pos king, const char chessboard[8][8]) {
   Pos knight = search_knight(chessboard);
-  int x = king_x + 2;
-  int y = king_x + 1;
+  int x = king.x + 2;
+  int y = king.x + 1;
   if (x == knight.x && y == knight.y)
     return 1;
-  x = king_x + 2;
-  y = king_y - 1;
+  x = king.x + 2;
+  y = king.y - 1;
   if (x == knight.x && y == knight.y)
     return 1;
-  x = king_x - 2;
-  y = king_y - 1;
+  x = king.x - 2;
+  y = king.y - 1;
   if (x == knight.x && y == knight.y)
     return 1;
-  x = king_x - 2;
-  y = king_y + 1;
+  x = king.x - 2;
+  y = king.y + 1;
   if (x == knight.x && y == knight.y)
     return 1;
-  x = king_x + 1;
-  y = king_y + 2;
+  x = king.x + 1;
+  y = king.y + 2;
   if (x == knight.x && y == knight.y)
     return 1;
-  x = king_x + 1;
-  y = king_y - 2;
+  x = king.x + 1;
+  y = king.y - 2;
   if (x == knight.x && y == knight.y)
     return 1;
-  x = king_x - 1;
-  y = king_y - 2;
+  x = king.x - 1;
+  y = king.y - 2;
   if (x == knight.x && y == knight.y)
     return 1;
-  x = king_x - 1;
-  y = king_y + 2;
+  x = king.x - 1;
+  y = king.y + 2;
   if (x == knight.x && y == knight.y)
     return 1;
   return 0;
@@ -138,27 +138,27 @@ Pos search_rook(const char chessboard[8][8]) {
         return (Pos) {x, y};
 }
 
-int check_with_a_rook(int king_y, int king_x, const char chessboard[8][8]) {
+int check_with_a_rook(Pos king, const char chessboard[8][8]) {
   Pos rook = search_rook(chessboard);
-  if (rook.y == king_y) {
-    if (rook.y < king_y) {
-      for (int y = rook.y + 1; y < king_y; y++) 
-        if (chessboard[y][king_x] == 'R')
+  if (rook.y == king.y) {
+    if (rook.y < king.y) {
+      for (int y = rook.y + 1; y < king.y; y++) 
+        if (chessboard[y][king.x] == 'R')
           return 1;
-    } else if (rook.y > king_y) {
-      for (int y = rook.y - 1; y > king_y; y--) 
-        if (chessboard[y][king_x] == 'R')
+    } else if (rook.y > king.y) {
+      for (int y = rook.y - 1; y > king.y; y--) 
+        if (chessboard[y][king.x] == 'R')
           return 1;
     }
   } 
-  if (rook.x == king_x) {
-    if (rook.x < king_x) {
-      for (int x = rook.x + 1; x < king_x; x++) 
-        if (chessboard[king_y][x] == 'R')
+  if (rook.x == king.x) {
+    if (rook.x < king.x) {
+      for (int x = rook.x + 1; x < king.x; x++) 
+        if (chessboard[king.y][x] == 'R')
           return 1;
-    } else if (rook.x > king_x){ 
-      for (int x = rook.x - 1; x > king_x; x--) 
-        if (chessboard[king_y][x] == 'R')
+    } else if (rook.x > king.x){ 
+      for (int x = rook.x - 1; x > king.x; x--) 
+        if (chessboard[king.y][x] == 'R')
           return 1;
     }
   } 
@@ -172,25 +172,25 @@ Pos search_pawn(const char chessboard[8][8]) {
         return (Pos) {x, y};
 }
 
-int check_with_a_pawn(int king_y, int king_x, const char chessboard[8][8]) {
+int check_with_a_pawn(Pos king, const char chessboard[8][8]) {
   Pos pawn = search_pawn(chessboard);
-  int y = king_y + 1;
-  int x = king_x + 1;
+  int y = king.y + 1;
+  int x = king.x + 1;
   if (y == pawn.y && x == pawn.x) 
     return 1;
 
-  y = king_y - 1;
-  x = king_x + 1;
+  y = king.y - 1;
+  x = king.x + 1;
   if (y == pawn.y && x == pawn.x)
     return 1;
 
-  y = king_y - 1;
-  x = king_x - 1;
+  y = king.y - 1;
+  x = king.x - 1;
   if (y == pawn.y && x == pawn.x)
     return 1;
 
-  y = king_y + 1;
-  x = king_x - 1;
+  y = king.y + 1;
+  x = king.x - 1;
   if (y == pawn.y && x == pawn.x)
     return 1;
   return 0;
@@ -198,13 +198,13 @@ int check_with_a_pawn(int king_y, int king_x, const char chessboard[8][8]) {
 
 bool check_to_the_king(const char chessboard[8][8]) {
   Pos king = search_king(chessboard);
-  int check_with_a_queen_1 = check_with_a_queen(king.y, king.x, chessboard);
-  int check_with_a_bishop_1 = check_with_a_bishop(king.y, king.x, chessboard);
-  int check_with_a_knight_1 = check_with_a_knight(king.y, king.x, chessboard);
-  int check_with_a_rook_1 = check_with_a_rook(king.y, king.x, chessboard);
-  int check_with_a_pawn_1 = check_with_a_pawn(king.y, king.x, chessboard);
+  int queen_checked = check_with_a_queen(king, chessboard);
+  int bishop_checked = check_with_a_bishop(king, chessboard);
+  int knight_checked = check_with_a_knight(king, chessboard);
+  int rook_checked = check_with_a_rook(king, chessboard);
+  int pawn_checked = check_with_a_pawn(king, chessboard);
 
-  if (check_with_a_queen_1 == 1 || check_with_a_bishop_1 == 1 || check_with_a_knight_1 == 1 || check_with_a_rook_1 == 1 ||  check_with_a_pawn_1 == 1)
+  if (queen_checked == 1 || bishop_checked == 1 || knight_checked == 1 || rook_checked == 1 ||  pawn_checked == 1)
     return true;
 
   return false;
